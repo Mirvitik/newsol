9a01b630b139594bfdebaceae4e7d503e409bd93
Mirvitik <zombieboy3k@gmail.com>
2025-03-14 18:23:38 +0300

    Add files via upload

--- lesson2/answer.py
+++ lesson2/answer.py
@@ -11,6 +11,10 @@ def answer():
          'mot': 'Всегда мечтал застрять на Марсе!', 'ready': 'True'}
     return render_template('auto_answer.html', params=d)
 
+@app.route('/distribution')
+def distribution():
+    list_ = ['Ридли Скотт', "Энди Уир", "Марк Уотни", "Венката Капур", "Тедди Сандерс", "Шон Бин"]
+    return render_template('distribution.html', params=list_)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
